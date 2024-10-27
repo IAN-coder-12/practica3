7a0bd94ed05997f4e96c4af4c54f381fe89dacdc
Ian <ianhdez2020@gmail.com>
2024-10-27 14:39:37 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML = "El parrafó cambió";
+}
+function myFunction2() {
+    document.getElementById("mix").innerHTML = '<img id="imagen" src="https://www.hoyesarte.com/constelac10n/files/2020/11/The-Chainsmokers-Something-Just-Like-This.jpg" width="250px">';
+}
