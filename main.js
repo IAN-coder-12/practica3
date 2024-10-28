55cb7a2e8ba95a3ab93327df6391c8056c5979f6
Ian <ianhdez2020@gmail.com>
2024-10-28 02:24:27 -0600

    proyecto1.2

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function myFunction(){
 function myFunction2() {
     document.getElementById("mix").innerHTML = '<img id="imagen" src="https://www.hoyesarte.com/constelac10n/files/2020/11/The-Chainsmokers-Something-Just-Like-This.jpg" width="250px">';
 }
+
+function myFunction3() {
+    document.getElementById("new").innerHTML = '<div class="artistas"><img src="https://www.billboard.com/wp-content/uploads/2024/05/Camila-Cabello-bb28-2018-feat-billboard-fgiuhyx-espanol-1548.jpg?w=942&h=623&crop=1" width="250px" id="imagen"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTq9hwkmC-l8zeKylDneupnSddylF22a9bCVQ&s" width="250px" id="imagen"></div> <div id="mix2"></div><button type="button" onclick="myFunction4()">Mix</button>';
+}
+function myFunction4() {
+    document.getElementById("mix2").innerHTML = '<img id="imagen" src="https://i.ytimg.com/vi/tcrTQUVkUe0/maxresdefault.jpg" width="250px">';
+}
